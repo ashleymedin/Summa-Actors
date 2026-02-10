791ea75b5c8d19eadfd6f4887942c73f60c6a322
ashleymedin <ashley.vanbeusekom@gmail.com>
2026-02-09 22:27:16 -0600

    update compile

--- build/build_scripts/build.sh
+++ build/build_scripts/build.sh
@@ -2,16 +2,17 @@
 
 # If compiling on a Digital Research Alliance of Canada cluster,
 # load the following modules:
-# module load StdEnv/2020
-# module load gcc/9.3.0
-# module load openblas/0.3.17
-# module load netcdf-fortran/4.5.2
+# module load StdEnv/2023
+# module load gcc/12.3
+# module load openblas/0.3.24
+# module load openmpi/4.1.5
+# module load netcdf-fortran/4.6.1
 
 # If compiling on Anvil, load the following modules:
-# module load gcc/11.2.0 
-# module load openblas 
-# module load openmpi 
-# module load netcdf-fortran
+# module load gcc/14.2.0
+# module load openmpi/4.1.6
+# module load openblas/0.3.17
+# module load netcdf-fortran/4.5.3
 
 
 # If a library cannot be found by Cmake, you can specify the path like so:
@@ -26,14 +27,14 @@ export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$HOME/Summa-Actors/utils/dependenci
 # cmake --build ./cmake_build --target all -j
 
 # -----------------------------------
-# If compiling V4 without sundials use the folowing
+# If compiling V4 without sundials use the following
 # -----------------------------------
   
 # cmake -B ./cmake_build -S .. -DUSE_V4=ON
 # cmake --build ./cmake_build --target all -j
 
 # -----------------------------------
-# If compiling V4 with sundials use the folowing (default)
+# If compiling V4 with sundials use the following (default)
 # -----------------------------------
 
 cmake -B ./cmake_build -S .. -DUSE_SUNDIALS=ON -DCMAKE_BUILD_TYPE=Release
