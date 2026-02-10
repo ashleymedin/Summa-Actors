9b86a377ed2f26c42bcd36b2c0417bb593d46f5c
ashleymedin <ashley.vanbeusekom@gmail.com>
2026-02-09 22:32:41 -0600

    fix install

--- utils/dependencies/install_caf.sh
+++ utils/dependencies/install_caf.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 INSTALL_DIR=$PWD/caf
 
-# Load the necessary modules (cluster dependent below work for anvil)
-# module load gcc/11.2.0 
-# module load netlib-lapack
-# module load netcdf-fortran
+# If compiling on a Digital Research Alliance of Canada cluster,
+# load the following modules:
+# module load StdEnv/2023
+# module load gcc/12.3
+# module load openblas/0.3.24
+# module load netcdf-fortran/4.6.1
 
-wget https://github.com/actor-framework/actor-framework/archive/refs/tags/1.0.2.tar.gz
-tar -xvf 1.0.2.tar.gz
+# If compiling on Anvil, load the following modules:
+# module load gcc/14.2.0
+# module load openblas/0.3.17
+# module load netcdf-fortran/4.5.3
+
+wget https://github.com/actor-framework/actor-framework/archive/refs/tags/1.1.0.tar.gz
+tar -xvf 1.1.0.tar.gz
 
 
 echo "Installing CAF to $INSTALL_DIR"
 
-cd actor-framework-1.0.2
+cd actor-framework-1.1.0
 ./configure --prefix=$INSTALL_DIR
 cd build
 make -j 8
